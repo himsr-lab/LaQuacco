b0633e7d59fb2c16ff1f4aeea3d572b3b5a20d66
Christian Rickert <rc.login@icloud.com>
2023-12-22 14:02:23 -0700

    read image metadata and pixel data

---
+++ start.py
@@ -0,0 +1,32 @@
+import os
+import tifffile
+import xmltodict
+from typing import Any, Dict
+
+FILES = [
+    r"C:\Users\Christian Rickert\Desktop\Polaris\100923 P9huP54-2 #04 B1A1P1_[8520,46568]_component_data.tif",
+    r"C:\Users\Christian Rickert\Desktop\MIBI\[FOV2-1] UCD134_bottom_sample_SA21-06560_2023-05-24_10.tif",
+]
+
+for file in sorted(FILES):
+    name = os.path.basename(file)
+    print(f"\n\tFILE: {name}", flush=True)
+
+    # open TIFF file to extract image information
+    with tifffile.TiffFile(file) as tif:
+        # use data from first series (of pages) only
+        series = tif.series
+        pages = series[0].shape[0]
+        # access pages of first series
+        for page in tif.pages[0:pages]:
+            # get page name
+            try:
+                page_name = page.tags["PageName"].value  # regular TIFF
+            except KeyError:
+                image_description = page.tags["ImageDescription"].value  # OME-TIFF
+                image_dictionary = xmltodict.parse(image_description)
+                vendor_id = next(iter(image_dictionary))  # first and only key
+                print(image_dictionary[vendor_id]["Name"])
+
+            # get pixel data
+            pixels = page.asarray()
