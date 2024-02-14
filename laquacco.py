c0c7b59f3ba69a2f9293fd663bf4ed2c4b129a07
Christian Rickert <rc.login@icloud.com>
2024-02-14 14:39:01 -0700

    add copyright

--- laquacco.py
+++ laquacco.py
@@ -1,3 +1,32 @@
+"""
+Copyright 2023 The Regents of the University of Colorado
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Author:     Christian Rickert <christian.rickert@cuanschutz.edu>
+Group:      Human Immune Monitoring Shared Resource (HIMSR)
+            University of Colorado, Anschutz Medical Campus
+
+Title:      LaQuacco
+Summary:    Laboratory Quality Control v1.0 (2024-02-14)
+DOI:        # TODO
+URL:        https://github.com/christianrickert/LaQuacco
+
+Description:
+
+# TODO
+"""
+
 import datetime
 import fnmatch
 import os
