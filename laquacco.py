0ea368a39a4588b6e559809dbfd30da46cba9183
Christian Rickert <rc.login@icloud.com>
2024-09-29 17:03:42 -0600

    updated

--- laquacco.py
+++ laquacco.py
@@ -18,7 +18,7 @@ Group:      Human Immune Monitoring Shared Resource (HIMSR)
             University of Colorado, Anschutz Medical Campus
 
 Title:      LaQuacco
-Summary:    Laboratory Quality Control v2.0 (2024-09-28)
+Summary:    Laboratory Quality Control v2.0 (2024-09-29)
 DOI:        # TODO
 URL:        https://github.com/christianrickert/LaQuacco
 
